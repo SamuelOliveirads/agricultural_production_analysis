f2427c54b49c5e41a0dbe9866caa83171140562d
samuel <samueloliveira32df@gmail.com>
2023-10-29 13:52:10 -0300

    agricultura_analysis (buid): move codes to db

--- src/load_data.py
+++ src/load_data.py
@@ -5,12 +5,10 @@ import os
 def load_data():
     connection_str = os.environ.get('DATABASE_URL')
     engine = create_engine(connection_str)
-    query_crop_data = "SELECT * FROM crop.crop_data;"
-    df_crop_data = pd.read_sql(query_crop_data, engine)
+    
+    df_crop_data = pd.read_sql("SELECT * FROM crop.crop_data;", engine)
+    df_cod_municipios = pd.read_sql("SELECT * FROM crop.cod_municipios;", engine)
 
-    cod_municipios = pd.read_json('../dataset/ibge_municipios.json')
-    cod_municipios = cod_municipios.rename(columns={'ibge_code': 'cod_municipio'})
-
-    commodities_data = df_crop_data.merge(cod_municipios, on='cod_municipio')
+    commodities_data = df_crop_data.merge(df_cod_municipios, on='cod_municipio')
 
     return commodities_data
